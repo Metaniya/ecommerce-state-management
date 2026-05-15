7fc1715bb2565f628d7af83b9460b9b85a2a0cf2
Metaniya Shiferaw <125639579+Metaniya@users.noreply.github.com>
2026-05-15 10:32:35 +0300

    Set up Redux Toolkit store and slices

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addToCart: (state, action) => {
+      const existingItem = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.cart.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+    },
+
+    increaseQuantity: (state, action) => {
+      const item = state.cart.find(
+        (item) => item.id === action.payload
+      );
+
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+
+    decreaseQuantity: (state, action) => {
+      const item = state.cart.find(
+        (item) => item.id === action.payload
+      );
+
+      if (item) {
+        item.quantity -= 1;
+      }
+
+      state.cart = state.cart.filter(
+        (item) => item.quantity > 0
+      );
+    },
+
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+  },
+});
+
+export const {
+  addToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
