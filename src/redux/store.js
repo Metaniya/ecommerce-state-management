737cf3b45c0e6974f0e3b30d144d1e927938a3ca
Metaniya Shiferaw <125639579+Metaniya@users.noreply.github.com>
2026-05-15 10:49:54 +0300

    Fix Redux Persist storage configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import {
+  persistStore,
+  persistReducer,
+} from "redux-persist";
+
+import storage from "redux-persist/es/storage";
+
 import cartReducer from "./cartSlice";
 import authReducer from "./authSlice";
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    auth: authReducer,
-  },
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  auth: authReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  rootReducer
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor =
+  persistStore(store);
